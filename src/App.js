d163b9d108d4a76798b32eb9a4e451e2f597e175
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-09 17:08:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from "react";
-import "./App.css" 
+import "./App.css";
 import About from './Components/About'
+import Navbar from './Components/Navbar';
+import Portfolio from './Components/Portfolio';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Service from './Components/Service';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 
 function App(){
   
     return (
-      <About />
-     
+      <Router>
+    <div className="App">
+      <Navbar />
+      <Switch>
+      <Route path="/service" component={Service} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/resume" component= {Resume} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      </Switch>
+    </div>
+    </Router>
     )
 }
 
