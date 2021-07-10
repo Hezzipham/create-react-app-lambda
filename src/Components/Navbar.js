d159341ac93dbd62d299e3252389121fc21c25ed
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-09 20:48:40 -0400

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import '../App.css';
-import * as ReactBootStrap from 'react-bootstrap';
+//import * as ReactBootStrap from 'react-bootstrap';
 
 function Navbar() {
   return (
-    <ReactBootStrap.Navbar collapseOnSelect expand="lg" variant="dark">
-  <ReactBootStrap.Container>
-  <ReactBootStrap.Navbar.Brand href="/">Hezzi Pham</ReactBootStrap.Navbar.Brand>
-  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootStrap.Nav className="me-auto">
-      <ReactBootStrap.Nav.Link href="/service">Service</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="/portfolio">Portfolio</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="/resume">Resume</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="/about">About Me</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="/contact">Contact Me</ReactBootStrap.Nav.Link>
-    </ReactBootStrap.Nav>
-  </ReactBootStrap.Navbar.Collapse>
-  </ReactBootStrap.Container>
-</ReactBootStrap.Navbar>
+    <div className="navbar"  >
+            <Link to='/'>
+            <p>Logo</p>
+            </Link>
+            <ul className="navlist" >
+            <Link to='/service'>
+                <li className="navtab">Services</li>
+            </Link>
+            <Link to='/portfolio'>
+                <li className="navtab">Portfolio</li> 
+             </Link> 
+             <Link to='/about'>
+                <li className="navtab">About Me</li>
+              </Link> 
+              <Link to='/resume'>
+                <li className="navtab">Resume</li>
+               </Link>
+               <Link to='/contact'>
+                <li className="navtab">Contact</li>
+              </Link>
+            </ul> 
+    </div>
+    
   );
 }
 export default Navbar;
