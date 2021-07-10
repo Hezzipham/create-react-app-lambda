ce14411c1ac45f9bf3db31a080038cbfa937d19c
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-09 21:36:33 -0400

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom';
-//import * as ReactBootStrap from 'react-bootstrap';
+import {Nav, Navbar} from 'react-bootstrap';
 
 function Navbar() {
   return (
-    <div className="navbar"  >
-            <Link to='/'>
-            <p>Logo</p>
-            </Link>
-            <ul className="navlist" >
-            <Link to='/service'>
-                <li className="navtab">Services</li>
-            </Link>
-            <Link to='/portfolio'>
-                <li className="navtab">Portfolio</li> 
-             </Link> 
-             <Link to='/about'>
-                <li className="navtab">About Me</li>
-              </Link> 
-              <Link to='/resume'>
-                <li className="navtab">Resume</li>
-               </Link>
-               <Link to='/contact'>
-                <li className="navtab">Contact</li>
-              </Link>
-            </ul> 
-    </div>
-    
+    <Navbar className="navbar" expand="md">
+      <Navbar.Brand herf='/'>Hezzi Pham</Navbar.Brand >
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav as="ul">
+            <Nav.Item as="li">
+                <Link to='/service' className="navtab">Services</Link>    
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Link to='/portfolio' className="navtab">Portfolio</Link>   
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Link to='/about' className="navtab">About Me</Link>     
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Link to='/resume' className="navtab">Resume</Link>    
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Link to="/contact" className="navtab">Contact </Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse> 
+  </Navbar>
   );
 }
 export default Navbar;
