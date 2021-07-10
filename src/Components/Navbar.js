6e97723d7ad891301e2f7b82cf162dc1a7628412
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-09 23:28:48 -0400

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,25 +7,15 @@ function Navi() {
   return (
     <Navbar collapseOnSelect className="navbar" variant="dark" expand="md">
       <Container>
-      <Navbar.Brand><Link id="logo" to='/'>Hezzi Pham</Link></Navbar.Brand >
+      <Navbar.Brand > <Link id="logo" to="/">Hezzi Pham</Link></Navbar.Brand >
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav as="ul" className="ml-auto" >
-            <Nav.Item as="li">
-                <Link to='/service' className="nav-link">Services</Link>    
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Link to='/portfolio' className="nav-link">Portfolio</Link>   
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Link to='/about' className="nav-link">About Me</Link>     
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Link to='/resume' className="nav-link">Resume</Link>    
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Link to="/contact" className="nav-link">Contact </Link>
-            </Nav.Item>
+          <Nav.Link as={Link} href="/service" to='/service' className="nav-link">Services</Nav.Link>
+          <Nav.Link as={Link} href="/portfolio"to='/portfolio' className="nav-link">Portfolio</Nav.Link>   
+          <Nav.Link as={Link} href="/about"to='/about' className="nav-link">About Me</Nav.Link>     
+          <Nav.Link as={Link} href="/resume"to='/resume' className="nav-link">Resume</Nav.Link>    
+          <Nav.Link as={Link} href="/contact" to="/contact" className="nav-link">Contact </Nav.Link>
           </Nav>
         </Navbar.Collapse> 
       </Container>
