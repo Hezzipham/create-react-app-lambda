576a9ccfe01ae8bdb1d0c0b2d26e0ed92912fc9b
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-10 16:39:31 -0400

    remove <Nav.Link>

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,25 +9,15 @@ function Navi() {
 
     <Navbar expanded={expanded} className="navbar" variant="dark" expand="md">
       <Container>
-      <Navbar.Brand > <Link id="logo" to="/">Home</Link></Navbar.Brand >
+      <Navbar.Brand > <Link id="logo" to="/">Hezzi Pham</Link></Navbar.Brand >
       <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav as="ul" className="ml-auto" >
-          <Nav.Link onClick={() => setExpanded(false)}>
-                <Link to='/service' className="nav-link">Services</Link>    
-            </Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)}>
-                <Link to='/portfolio' className="nav-link">Portfolio</Link>   
-            </Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)}>
-                <Link to='/about' className="nav-link">About Me</Link>     
-            </Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)}>
-                <Link to='/resume' className="nav-link">Resume</Link>    
-            </Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)}>
-                <Link to="/contact" className="nav-link">Contact </Link>
-            </Nav.Link>
+                <Link as={Nav.Link}  onClick={() => setExpanded(false)} to='/service' className="nav-link">Services</Link>    
+                <Link as={Nav.Link}  onClick={() => setExpanded(false)} to='/portfolio' className="nav-link">Portfolio</Link>   
+                <Link as={Nav.Link}  onClick={() => setExpanded(false)} to='/about' className="nav-link">About Me</Link>     
+                <Link as={Nav.Link}  onClick={() => setExpanded(false)} to='/resume' className="nav-link">Resume</Link>    
+                <Link as={Nav.Link}  to="/contact" className="nav-link">Contact </Link>
           </Nav>
         </Navbar.Collapse> 
       </Container>
