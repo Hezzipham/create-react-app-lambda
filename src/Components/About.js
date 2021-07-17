a7947bb3d2e052ee235d42d52440b0be790e0301
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-17 17:02:19 -0400

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,7 @@ function About() {
             <Row>
                 <Col xs={12} md={6}>
                     <h2>Who Am I?</h2>
-                    <p>My real name is Hien Thu Pham. Hien is a very popular name but I prefer mynick name "Hezzi" since it is more unique and I never have to be Hien A, B, C anymore haha </p>
+                    <p>My real name is Hien Thu Pham. Hien is a very popular name but I prefer my nickname "Hezzi" because it's more unique and I never have to be Hien A, B, C anymore haha </p>
                     <h3>My Background</h3>
                     <p><b>Business background:</b> I have earned a Finance Bachelor's Degree and an MBA, I also have a real estate license in North Carolina.</p>
                         <p><b>Expertise in the beauty industry:</b> I have been a licensing cosmetologist in California and working in customer service and retail for 5 years now.</p>
