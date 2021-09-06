d21af3428b7ee138164be1d58eca86292d07b272
Hezzi Pham <52872927+Hezzipham@users.noreply.github.com>
2021-09-06 13:40:43 -0400

    resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -17,43 +17,10 @@ function Resume() {
 
   {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
   <Row className="resumbody">
-    
-    <Col xs={12} md={8}>
-    <h4>PROJECT</h4>
-    <h5>Hezzipham.com - <a target="_blank" rel="noreferrer noopener" href="https://github.com/Hezzipham/hezziwebsite">GitHub</a> | <a target="_blank" rel="noreferrer noopener" href="https://hezzipham.com/">Live</a></h5>
-    <p>Personal Website using React, React Router, Bootstrap, Sass, and netlify-lambda together in a Netlify Dev workflow. Implement react hooks to fetch data to build an e-commerce shopping cart and a  responsive portfolio. </p>
-    <h5>Bunpeti.com - <a target="_blank" rel="noreferrer noopener" href="https://github.com/Hezzipham/bunpeti">GitHub</a> | <a target="_blank" rel="noreferrer noopener" href="https://bunpeti.com/">Live</a></h5>
-    <p>Using HTML, Sass, TypeScript and  jQuery to create fun animation, reponsive website to publicize Bunpeti youtube channel and display some affiliate marketing products.</p>
-    <h5>Libraryofgames.org - <a target="_blank" rel="noreferrer noopener" href="https://github.com/Hezzipham/halloworld">GitHub</a> | <a target="_blank" rel="noreferrer noopener" href="https://libraryofgames.org/">Live</a></h5>
-    <p>A nonprofit organization website to promote Vietnamese folk games. Using html5, CSS, vanilla JavaScript, and free hosting with GitHub and Netlify</p>
-    <h5>Domino Media - <a href="https://github.com/Hezzipham/Dominomedia" target="_blank" rel="noreferrer noopener">GitHub</a> | <a target="_blank" rel="noreferrer noopener" href="https://domino.chienvu.com/" >Live</a></h5>
-    <p>A Digital Agency Website, implementing JavaScript, Css, Hugo meghna bundle toghether with Forestry to build a bilingual web app, easy to maintain by client</p>
-    <h5>Dinodaycare.com - <a target="_blank" rel="noreferrer noopener" href="http://dinodaycare.com/">Live</a> </h5>
-    <p>A preschool responsive website, where clients can fill in forms, chat support online, and navigate to different documentation. Using WordPress, Linux Hosting with cPanel</p>
-    <h4>EXPERIENCE</h4>
-    <h5>Youtube.com - Bunpeti Channel | April 2020 – Present</h5>
-    <p><b>Youtube partner </b> Content creator Research and create content for a pet rabbit channel.</p>
-    <h5>ULTA Beauty | September 2018 – April 2020</h5>
-     <p><b>Salon, Cosmetics & Fragrance Inc </b>Cosmetologist Perform beauty services which were rated 4.8/5 by more than 200 clients. Achieve the first runner selling Paul Mitchell Hair Products in Cumberland County after three months of working.</p>
-    
-    <h5>DINO Daycare & Preschool, San Diego, California|April 2016 – July 2017</h5>
-    <p><b>Daycare Center, Internal Consultant </b>Created and managed the Daycare’s website and social media platforms including Yelp, Facebook, Instagram with more than 5,000 website views and 25,000 followers within the first year.</p>
-    
-    <h5>Good Samaritan Medical Dental Ministry, Riverside, CA| January 2016 - April 2016</h5>
-    <p><b>Nonprofit Organization, Operation Volunteer </b>Researched and classified grant opportunities from potential foundation Coordinately planned and held fundraising events.</p>
-    
-    </Col>
-    <Col xs={12} md={4}>
-      <h4>CERTIFICATION</h4>
-      <a target="_blank" rel='noreferrer noopener' href="https://www.freecodecamp.org/certification/fccc165c594-3b23-412c-84f6-e770f53864b5/responsive-web-design"><h5>Responsive Web Design</h5></a>
-      <p><FaFreeCodeCamp></FaFreeCodeCamp> Freecodecamp.org</p>
-      <a target="_blank" rel='noreferrer noopener' href="https://www.freecodecamp.org/certification/fccc165c594-3b23-412c-84f6-e770f53864b5/javascript-algorithms-and-data-structures"><h5>JavaScript Algorithms and Data Structures</h5></a>
-      <p><FaFreeCodeCamp></FaFreeCodeCamp> Freecodecamp.org</p>
-      <a target="_blank" rel='noreferrer noopener' href="https://www.freecodecamp.org/certification/fccc165c594-3b23-412c-84f6-e770f53864b5/front-end-libraries"><h5>Front End Libraries</h5></a>
-      <p><FaFreeCodeCamp></FaFreeCodeCamp> Freecodecamp.org</p>
-      <a target="_blank" rel='noreferrer noopener' href="https://www.udemy.com/certificate/UC-1624b478-ac31-41f2-acc4-5c5851e22d8a/"><h5>Understanding TypeScript</h5></a>
-      <p><SiUdemy></SiUdemy> Udemy.com</p>
-      <h4>TECHNICAL SKILL</h4>
+  <Col xs={12} md={4}>
+  <h4>EXECUTIVE SUMMARY</h4>
+  <p>Meticulous full stack web developer, dedicated to learn new technology and solve challenging problems with strong technical skills, proactive mindset and positive attitude. </p>
+  <h4>TECHNICAL SKILL</h4>
       <p><b> <span>HTML <FaHtml5></FaHtml5> |</span>
       <span> CSS <FaSass></FaSass> |</span>
       <span> JavaScript <FaJsSquare></FaJsSquare> |</span>
@@ -68,6 +35,31 @@ function Resume() {
       <span> Bootstrap <FaBootstrap></FaBootstrap> |</span>
       <span> jQuery <SiJquery></SiJquery></span>
       </b></p>
+
+      <ul><h4>CERTIFICATION</h4>
+    <li>
+      <a target="_blank" rel='noreferrer noopener' href="https://www.udemy.com/certificate/UC-57e38dfa-54ed-425a-8afe-de0e47fa6008/"><h5>The Python Pro Bootcamp</h5></a>
+    </li>
+    <li>
+      <a target="_blank" rel='noreferrer noopener' href="https://www.udemy.com/certificate/UC-d5333fa2-f6ae-41b8-af3b-34202ef978c6/"><h5>The Complete SQL Bootcamp</h5></a>
+    </li>
+    <li>
+      <a target="_blank" rel='noreferrer noopener' href="https://www.udemy.com/certificate/UC-1624b478-ac31-41f2-acc4-5c5851e22d8a/"><h5>Understanding TypeScript</h5></a>
+    </li> 
+    <li>
+      <a target="_blank" rel='noreferrer noopener' href="https://www.freecodecamp.org/certification/fccc165c594-3b23-412c-84f6-e770f53864b5/javascript-algorithms-and-data-structures"><h5>JavaScript Algorithms and Data Structures</h5></a>
+    </li>
+    <li>
+      <a target="_blank" rel='noreferrer noopener' href="https://www.freecodecamp.org/certification/fccc165c594-3b23-412c-84f6-e770f53864b5/front-end-libraries"><h5>Front End Libraries</h5></a>
+    </li>
+    <li>
+      <a target="_blank" rel='noreferrer noopener' href="https://www.freecodecamp.org/certification/fccc165c594-3b23-412c-84f6-e770f53864b5/responsive-web-design"><h5>Responsive Web Design</h5></a>
+    </li>
+    <li>
+      <a target="_blank" rel='noreferrer noopener' href="https://www.linkedin.com/learning/scrum-the-basics"><h5>Scrum: The Basics</h5></a>
+    </li>
+      </ul>
+      
       <h4>EDUCATION</h4>
             <h5>MBA</h5>
             <p><b>California Baptist University</b>,Riverside, CA</p>
@@ -83,6 +75,26 @@ function Resume() {
             <li>Animation</li>
             <li>Youtube Partner</li>
     </Col>
+    
+    <Col xs={12} md={8}>
+    <h4>PROFESSIONAL PROJECT</h4>
+    <h5>Personal Website - <a target="_blank" rel="noreferrer noopener" href="https://github.com/Hezzipham/hezziwebsite">GitHub</a> | <a target="_blank" rel="noreferrer noopener" href="https://hezzipham.com/">Live</a></h5>
+    <p>A website to display personal information, portfolio, and estimate services price quotation. Design with React, Bootstrap, SASS and using yarn package to deploy on Netlify </p>
+    <h5>Libraryofgames.org - <a target="_blank" rel="noreferrer noopener" href="https://github.com/Hezzipham/halloworld">GitHub</a> | <a target="_blank" rel="noreferrer noopener" href="https://libraryofgames.org/">Live</a></h5>
+    <p>Python Flask app where people can upload, download books and log in to delete data. Using SQLite to store data, Flask WTF to build an interactive user interface.</p>
+    <h5>Flight Deals - <a target="_blank" rel="noreferrer noopener" href="https://github.com/Hezzipham/Flight_Deals/">GitHub</a> </h5>
+    <p>Python app using Kiwi Partners Tequila API to search for the best flight deals within 6 months. Use the Twilio API to automatically send an SMS with information to book the flight. </p>
+    <h4>EXPERIENCE</h4>
+    <h5><a target="_blank" rel="noreferrer noopener" href="https://www.bunpeti.com/">Bun Pet Inc | Sep 2020 – Present</a></h5> 
+    <p><b>Founder </b> Developed a website to create content about pet care,  recommending pet supplies,  which gain 3000 subscribers, create 80 youtube videos, gather and answer 5000 questions.   </p>
+    <h5><a target="_blank" rel="noreferrer noopener" href="https://www.chienvu.com/">Domino Media | September 2017 – April 2020</a></h5>
+     <p><b>Web Deverloper</b> Reinforced the brand by redesigning the company identity and build a bilingual portfolio website, attracts more than 2000 clients a year.</p>
+    
+    <h5><a target="_blank" rel="noreferrer noopener" href="http://dinodaycare.com">DINO Daycare & Preschool | April 2016 – July 2017</a></h5>
+    <p><b>PR-Marketing Manager</b>Created and managed the Daycare’s website and social media platforms including Yelp, Facebook, Instagram with more than 5,000 website views and 25,000 followers within the first year.</p>
+
+    </Col>
+    
   </Row>
 
   {/* Columns are always 50% wide, on mobile and desktop */}
