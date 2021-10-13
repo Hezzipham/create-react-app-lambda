1f7ddab5170f7a3fc7ce4679ddc229b7c5bc2d89
Hezzi Pham <52872927+Hezzipham@users.noreply.github.com>
2021-10-13 11:44:46 -0600

    repeat(num)

--- src/Components/hello.js
+++ src/Components/hello.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { FaHistory } from 'react-icons/fa';
 import '../App.css';
 
 
-class Hello extends React.Component {
+class Hi extends React.Component {
+
     render(){
-        return (<h1>Hello {this.props.from} to {this.props.to}</h1>
+        let bangs = "!".repeat(this.props.bangs)
+        return (
+        <h1>Hello {this.props.from} to {this.props.to} {bangs}</h1>
+            )
+    }
+
+}
+
+class Hello extends React.Component {
+
+    render(){
+        return ( 
+        <div>
+            <Hi
+        from="Hezzi"
+        to="World" 
+        num ={3}
+        data={[1,2,3,4,5]}
+        isFunny = {true}
+        bangs={5}
+        />
+        <Hi
+        from="Hahah"
+        to="Hihi" 
+        num ={3}
+        data={[1,2,3,4,5]}
+        isFunny = {true}
+        bangs={2}
+        />
+        </div>
+        
             )
     }
 }
