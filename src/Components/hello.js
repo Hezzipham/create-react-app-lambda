c527a6b418fa244f7fd828579b552b988252872a
Hezzi Pham <52872927+Hezzipham@users.noreply.github.com>
2021-10-13 11:53:30 -0600

    image

--- src/Components/hello.js
+++ src/Components/hello.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FaHistory } from 'react-icons/fa';
 import '../App.css';
 
 
@@ -8,7 +7,11 @@ class Hi extends React.Component {
     render(){
         let bangs = "!".repeat(this.props.bangs)
         return (
-        <h1>Hello {this.props.from} to {this.props.to} {bangs}</h1>
+           <div>
+               <h1>Hello {this.props.from} to {this.props.to} {bangs}</h1>
+               <img scr={this.props.img} alt="No"/>
+           </div> 
+        
             )
     }
 
@@ -25,16 +28,9 @@ class Hello extends React.Component {
         num ={3}
         data={[1,2,3,4,5]}
         isFunny = {true}
-        bangs={5}
-        />
-        <Hi
-        from="Hahah"
-        to="Hihi" 
-        num ={3}
-        data={[1,2,3,4,5]}
-        isFunny = {true}
-        bangs={2}
-        />
+        bangs= {5}
+        img= "https://images.unsplash.com/photo-1633983054677-4c9cdd4e16cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" />
+
         </div>
         
             )
