937f687c6342a4451c5b86c2d12e421598b94a71
Hezzi Pham <52872927+Hezzipham@users.noreply.github.com>
2021-10-14 06:27:39 -0600

    static defaultProps

--- src/Components/hello.js
+++ src/Components/hello.js
@@ -3,12 +3,12 @@ import '../App.css';
 
 
 class Hi extends React.Component {
-
+    static defaultProps = { from: "Hezzi", bangs:3 }
     render(){
         let bangs = "!".repeat(this.props.bangs)
         return (
            <div>
-               <h1>Hello {this.props.from} to {this.props.to} {bangs}</h1>
+               <h1>Hello from {this.props.from} to {this.props.to} {bangs}</h1>
                <img src={this.props.img} alt="No"/>
            </div> 
         
@@ -23,12 +23,10 @@ class Hello extends React.Component {
         return ( 
         <div>
             <Hi
-        from="Hezzi"
         to="World" 
         num ={3}
         data={[1,2,3,4,5]}
         isFunny = {true}
-        bangs= {5}
         img= "https://images.unsplash.com/photo-1633983054677-4c9cdd4e16cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" />
 
         </div>
